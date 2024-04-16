5d1763a733460904d3d319c108d2d331b36ae426
hoaqdzink2312 <vinhnh.2312@gmail.com>
2024-04-16 14:09:36 +0700

    test

--- src/admin/pages/managers.js
+++ src/admin/pages/managers.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 class managers extends Component {
   render() {
-    return <div>managers</div>;
+    return <div>managers1</div>;
   }
 }
+
 export default managers;
