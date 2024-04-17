191aa991adb858c104b91656179e4ce0cdb88003
hoaqdzink2312 <vinhnh.2312@gmail.com>
2024-04-17 14:40:30 +0700

    add fronted student

--- src/admin/pages/student.js
+++ src/admin/pages/student.js
@@ -1,8 +1,50 @@
 import React from 'react'
-
+import '../css/stdeunt.css'
 function student(){
   return (
-    <div>student</div>
+    <div>
+      <a href="admin/student">Thông tin xin viên</a> ||
+      <a href="admin/student/list">Danh sách sinh viên</a>
+      <hr />
+      <hr />
+      <h1 className='text_student'>Thông tin sinh viên</h1>
+      <form action="" className='form_student'>
+        <div className='groupInp'>
+          <label htmlFor="fullname">Fullname: </label>
+          <input type="text" name='fullname' />
+        </div>
+        <div className='groupInp'>
+          <label htmlFor="mssv">MSSV: </label>
+          <input type="text" name='id' />
+        </div>
+        <div className='groupInp'>
+          <label htmlFor="birthday">Ngày sinh: </label>
+          <input type="date" name='birthday' />
+        </div>
+        <div className='groupInp'>
+          <label htmlFor="gender">Giới tính: </label> <br />
+          Nam <input type="radio" value={true} /> 
+          Nữ <input type="radio" value={false} /> 
+        </div>
+        <div className='groupInp'>
+          <label htmlFor="address">Địa chỉ: </label>
+          <input type="text" name='address' />
+        </div>
+        <div className='groupInp'>
+          <label htmlFor="phone">Phone: </label>
+          <input type="text" name='phone' />
+        </div>
+        <div className='groupInp'>
+          <label htmlFor="startYear">Năm nhập học: </label>
+          <input type="number" name='startYear' />
+        </div>
+        <button>Thêm</button>
+        <button>Sửa</button>
+        <button>Xóa</button>
+        <button>Reset</button>
+        
+      </form>
+    </div>
   )
 }
 
